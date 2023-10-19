0ec3518f819fd819c161c0873a5ef6efeb77a078
oliviawhiteman <oliviarosew04@gmail.com>
2023-10-19 19:05:44 +1300

    update

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,37 @@
 
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+let firstRun = true
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+let tree = [];
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+
+var Xtreearray = [50*10,300*10,320*10];
+var Ytreearray = [100*10,500*10,300*10];
+
+
+function draw_one_frame(vocal,bass,drum,other){
+  if (firstRun){
+    rectMode (CENTER);
+    tree.push(loadImage('pine0.png'))
+    tree.push(loadImage('pine1.png'))
+    tree.push(loadImage('pine2.png'))
+    tree.push(loadImage('pine3.png'))
+
+
+    firstRun = false
+  }
+  background(225)
+  
+  
+  var VocalFrame = int(map(drum,0,100,0,5));
+  console.log(VocalFrame);
+  push();
+  scale(0.1)
+  image(tree[VocalFrame],Xtreearray[0],Ytreearray[0])
+  image(tree[VocalFrame],Xtreearray[1],Ytreearray[1])
+  image(tree[VocalFrame],Xtreearray[2],Ytreearray[2])
+  pop();
+
+
+
+  
 }
